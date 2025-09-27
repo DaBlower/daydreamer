b9b809e46d4f7a867c5041936cc6d1c67a6e7b41
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-09-27 23:02:08 +1000

    add pause and shop_test scene

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,31 +8,33 @@ const JUMP_VELOCITY = -600.0
 @onready var point_light_2d: PointLight2D = $PointLight2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if !Global.get_game_status(): # true = paused, false = normal
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-	
-	# handle candle
-	if Input.is_action_just_pressed("increase_candle"):
-		light_radius += 0.5
-		GlobalTime.set_radius(light_radius)
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Handle jump.
+		if Input.is_action_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		
+		# handle candle
+		if Input.is_action_just_pressed("increase_candle"):
+			light_radius += 0.5
+			GlobalTime.set_radius(light_radius)
+		
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("left", "right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
 
 func _process(delta: float) -> void:
-	light_radius = GlobalTime.update_radius(delta)
-	point_light_2d.texture_scale = light_radius
+	if !Global.get_game_status():
+		light_radius = GlobalTime.update_radius(delta)
+		point_light_2d.texture_scale = light_radius
 	
 	
