45613489a79d4575239957b09ae623242dd0cbbb
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-09-27 23:50:21 +1000

    add timer in top left

--- scripts/time.gd
+++ scripts/time.gd
@@ -6,6 +6,7 @@ extends Node
 var max_radius: float = 0.2
 var min_radius: float = 0.0
  
+signal time_changed(new_time: float)
 
 func get_time() -> float:
 	# remaining time proportional to distance between min and max
@@ -15,12 +16,14 @@ func subtract_time(time: float):
 	var ratio = time / duration
 	current_radius -= (max_radius - min_radius) * ratio
 	current_radius = clamp(current_radius, min_radius, max_radius)
+	emit_signal("time_changed", get_time())
 	
 func update_radius(delta: float):
 	# reduce radius every second
 	current_radius -= (max_radius - min_radius) / duration * delta
 	current_radius = clamp(current_radius, min_radius, max_radius)
 	print(get_time())
+	emit_signal("time_changed", get_time())
 	return current_radius
 	
 func get_radius() -> float:
@@ -28,4 +31,5 @@ func get_radius() -> float:
 	
 func set_radius(radius: float) -> void:
 	current_radius = clamp(radius, min_radius, max_radius)
+	emit_signal("time_changed", get_time())
 	print(get_time())
