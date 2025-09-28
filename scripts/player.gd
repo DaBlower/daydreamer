957fa9cc44ae92128ec30b54ea0a64a0b3565090
Benjamin Yang <benjamin.yang12@outlook.com>
2025-09-28 14:00:53 +1000

    add portal to map

--- scripts/player.gd
+++ scripts/player.gd
@@ -86,6 +86,10 @@ func _physics_process(delta: float) -> void:
 			
 		move_and_slide()
 		
+		for i in get_slide_collision_count():
+			var collision = get_slide_collision(i)
+			if collision.get_collider().is_in_group("Kill"):
+				die()
 
 func die() -> void:
 	position = Vector2(167, 470)
